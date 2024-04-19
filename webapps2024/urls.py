f360400faed4c59b2a9d04e30b804d7d5b1c01a3
Chris Park <dev.chris.kr@gmail.com>
2024-04-19 20:48:40 +0900

    Adding redirect for root access

--- webapps2024/urls.py
+++ webapps2024/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include
+from django.urls import path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', include('register.urls')),
     path('payapp/', include('payapp.urls')),
+    path('', RedirectView.as_view(url='/register/user-list', permanent=True))
 ]
